da035689097cef34696e38f370b765bf83599350
harithakrishk <harithakrish965@gmail.com>
2021-04-25 16:54:33 +0530

    lo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,29 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import {MatButtonModule} from '@angular/material/button';
+import {MatInputModule} from '@angular/material/input';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { StudentComponent } from './student/student.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { SampleComponent } from './sample/sample.component';
+import { BloodBankComponent } from './blood-bank/blood-bank.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    StudentComponent
+    StudentComponent,
+    SampleComponent,
+    BloodBankComponent,
+    
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatButtonModule,
+    BrowserAnimationsModule,
+    MatInputModule
   ],
   providers: [],
   bootstrap: [AppComponent]
